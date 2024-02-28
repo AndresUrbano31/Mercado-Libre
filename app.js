ffb45464018d1f9f0b9cda6d3f5867d283ce95c6
AndresUrbano31 <aurbano535@gmail.com>
2024-02-27 21:57:34 -0500

    cambio

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3001;
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  });
+
 
   app.get('/', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/home.html'));});
   app.get('/register', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/register.html'));});
   app.get('/login', (req, res) =>{res.sendFile(path.resolve(__dirname, './view/login.html'));});
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
